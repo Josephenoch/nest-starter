c96188577c16235e92954b6e2d9a0a9d979ee1b5
Joseph Enoch <enochjoseph01@gmail.com>
2023-04-04 18:42:21 +0100

    add the controllers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-@Controller({})
-export class AuthController {}
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('signUp')
+  signUp() {
+    return 'I am signUp';
+  }
+  @Post('login')
+  login() {
+    return 'I am login';
+  }
+}
