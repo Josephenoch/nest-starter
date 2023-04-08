c7e5290e5275b97f33c287810c15dafc6236989f
Joseph Enoch <enochjoseph01@gmail.com>
2023-04-08 19:09:21 +0100

    implement jwt encoding

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,16 @@ import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDTO } from "./dto";
 import * as argon from "argon2";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
   async login(dto: AuthDTO) {
     // console.log(dto)
     try {
@@ -19,8 +25,7 @@ export class AuthService {
       if (!pwdCompare) {
         throw new ForbiddenException("Credentials Incorrect");
       }
-      delete user.password;
-      return user;
+      return await this.signToken(user.id, user.email);
     } catch (err) {
       if (err.code === "P2002") {
         throw err;
@@ -46,5 +51,20 @@ export class AuthService {
       }
       throw err;
     }
+  }
+
+  async signToken(userID: number, email: string) {
+    const payload = {
+      sub: userID,
+      email,
+    };
+    const access_token = await this.jwt.signAsync(payload, {
+      secret: this.config.get("JWT_SECRET"),
+      expiresIn: "1w",
+    });
+
+    return {
+      access_token,
+    };
   }
 }
