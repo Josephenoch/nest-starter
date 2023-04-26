cb7bbc22686a56f58e8c3ccd2add5a801cbb045d
Joseph Enoch <enochjoseph01@gmail.com>
2023-04-26 18:17:44 +0100

    chore: reinitialize app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,6 @@
-import { Module } from "@nestjs/common";
-import { AuthModule } from "./auth/auth.module";
-import { UserModule } from "./user/user.module";
-import { BookmarkModule } from "./bookmark/bookmark.module";
-import { PrismaModule } from "./prisma/prisma.module";
-import { ConfigModule } from "@nestjs/config";
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [
-    AuthModule,
-    UserModule,
-    BookmarkModule,
-    PrismaModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-  ],
+  imports: []
 })
 export class AppModule {}
