cfc8c697b791a04b11c9915d77644d0680bdd5f7
Joseph Enoch <enochjoseph01@gmail.com>
2023-04-29 01:59:43 +0100

    create functions

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('signin')
+  signIn() {
+    return this.authService.signIn();
+  }
+  @Post('signUp')
+  signUp() {
+    return this.authService.signUp();
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  signIn() {
+    return 'Signed In';
+  }
+
+  signUp() {
+    return 'Signed Up';
+  }
+}
