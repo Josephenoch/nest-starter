ca58c66c867a1f06d5141a172c89b08208576d15
Joseph Enoch <enochjoseph01@gmail.com>
2023-05-11 16:36:56 +0100

    chore: get started with auth logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDTO } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('signin')
-  signIn() {
+  signIn(@Body() dto: AuthDTO) {
+    console.log(dto.email);
     return this.authService.signIn();
   }
   @Post('signUp')
