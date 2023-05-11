d24547ea1fdc0bfd9fe2f0a8332a4a08d90d584f
Joseph Enoch <enochjoseph01@gmail.com>
2023-05-11 15:50:12 +0100

    chore: complete prisma modules

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://josephenoch:@localhost:5432/bookmark_app?schema=public',
+        },
+      },
+    });
+  }
+}
