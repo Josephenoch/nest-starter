72b9e0c729db43e242a603dcd88efa65cd04a94e
Joseph Enoch <enochjoseph01@gmail.com>
2023-05-11 15:33:51 +0100

    chore: create initial migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
-import { Module } from "@nestjs/common";
-import { UserModule } from "./user/user.module";
-import { BookmarkModule } from "./bookmark/bookmark.module";
-import { AuthModule } from "./auth/auth.module";
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { UserModule } from './user/user.module';
 
