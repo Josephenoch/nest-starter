0cd88a7d4c5f901dcade6b12499998e5fa832fd6
Joseph Enoch <enochjoseph01@gmail.com>
2023-05-16 14:52:13 +0100

    chore: create the prisma schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [UserModule, AuthModule],
-  controllers: [AuthController],
-  providers: [AuthService],
 })
 export class AppModule {}
