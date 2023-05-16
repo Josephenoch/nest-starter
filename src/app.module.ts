562d79937346d78136ada19c35f85f5f09e2a5de
Joseph Enoch <enochjoseph01@gmail.com>
2023-05-16 14:01:27 +0100

    chore: restart project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    UserModule,
-    BookmarkModule,
-    AuthModule,
-    PrismaModule,
-  ],
+  imports: [UserModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
