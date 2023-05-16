a1c485f729bd321c68578b3c2e5bf16e09536a35
Joseph Enoch <enochjoseph01@gmail.com>
2023-05-16 15:40:12 +0100

    chore: implement the dtos and validation pipes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginAuthDTO, SignUpAuthDTO } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signUp() {
-    return this.authService.signUp();
+  signUp(@Body() dto: SignUpAuthDTO) {
+    return this.authService.signUp(dto);
   }
 
   @Post('login')
-  login() {
-    return this.authService.login();
+  login(@Body() dto: LoginAuthDTO) {
+    return this.authService.login(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { LoginAuthDTO, SignUpAuthDTO } from './dto';
 
 @Injectable()
 export class AuthService {
-  signUp() {
+  signUp(dto: SignUpAuthDTO) {
     return 'signed up';
   }
 
-  login() {
+  login(dto: LoginAuthDTO) {
+    console.log(dto);
     return 'logged in';
   }
 }
