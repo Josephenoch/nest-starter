2f48952a634fbc3085238189eb2beed112a39238
Joseph Enoch <enochjoseph01@gmail.com>
2023-05-17 20:20:57 +0100

    chore: update files

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -8,7 +8,10 @@ export class PrismaService extends PrismaClient {
     super({
       datasources: {
         db: {
-          url: config.get('DATABASE_URL'),
+          url:
+            config.get('ENV') === 'production'
+              ? config.get('PROD_DATABASE_URL')
+              : config.get('DEV_DATABASE_URL'),
         },
       },
     });
