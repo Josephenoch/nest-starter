0eb30971e2f49725853fc0671976f83df1c8bff9
Joseph Enoch <enochjoseph01@gmail.com>
2023-05-18 15:44:42 +0100

    chore: updates

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Headers } from '@nestjs/common';
 
 @Controller('/')
 export class AppController {
   @Get()
-  getHello() {
+  getHello(@Headers() header: any) {
+    console.log(header);
     return 'Hello World';
   }
 }
