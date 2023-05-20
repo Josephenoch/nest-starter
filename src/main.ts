7ce029118f967ade861a63e3956f468aabad2250
Joseph Enoch <enochjoseph01@gmail.com>
2023-05-20 01:14:39 +0100

    chore: new-fixes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  console.log(process.env.DATABASE_URL);
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 5000);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guards';
 
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private prisma: PrismaService,
+  ) {}
+
+  @Get('me')
+  @UseGuards(JwtGuard)
+  me(@Req() req: Request) {
+    return this.prisma.user.findUnique({
+      where: {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        id: req.user,
+      },
+    });
+  }
 }
