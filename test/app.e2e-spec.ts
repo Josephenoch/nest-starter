99872b680b151c5a00a29a5c00e7063d286381bd
Joseph Enoch <enochjoseph01@gmail.com>
2023-05-20 17:11:43 +0100

    chore: start writing tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,15 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-
+import { PrismaService } from '../src/prisma/prisma.service';
+// import * as pactum from "pactum"
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
+
+  //  we create a test app
+  // we pass in the validation pipe so auth pipes would work
+  // we get prisma and clean the db, ie. clear the data in the db
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -11,12 +17,44 @@ describe('App e2e', () => {
     app = moduleRef.createNestApplication();
     app.use(new ValidationPipe({ whitelist: true }));
     await app.init();
+    prisma = app.get(PrismaService);
+    await prisma.cleanDB();
   });
 
   afterAll(() => {
     app.close();
   });
-  test.todo('should pass');
+
+  describe('Auth', () => {
+    describe('Sign up', () => {
+      test.todo('Should Sign Up');
+    });
+    describe('Sign in', () => {
+      test.todo('Should Sign In');
+    });
+  });
+
+  describe('User', () => {
+    // queries
+    describe('Get me', () => {});
+
+    // mutations
+    describe('Edit me', () => {});
+  });
+
+  describe('Bookmark', ()=>{
+
+    // queries
+    describe('Get bookmark by id', () => {});
+    describe('Get bookmarks', () => {});
+
+    // mutations 
+    describe('Create bookmark', () => {});
+    describe('Delete bookmark', () => {});
+    describe('Edit bookmark', () => {});
+
+
+  });
 });
 
 // https://youtu.be/ZsQnAuh3tZU
